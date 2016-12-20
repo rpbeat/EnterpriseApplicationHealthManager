08d437654174556e1a056c91ab36e587b9553bba
ruben <2140816@MY.IPLEIRIA.PT>
2016-12-20 15:54:04 +0000

    Domingues dfdgf

--- EnterpriseApplicationHealthManager-war/src/java/web/AdministratorManager.java
+++ EnterpriseApplicationHealthManager-war/src/java/web/AdministratorManager.java
@@ -11,6 +11,7 @@ import dtos.CuidadorDTO;
 import dtos.ProfissionalSaudeDTO;
 import dtos.UtenteDTO;
 <<<<<<< HEAD
+<<<<<<< HEAD
 import ejbs.AdministratorBean;
 import ejbs.CuidadorBean;
 import ejbs.ProfissionalSaudeBean;
@@ -106,6 +107,84 @@ public class AdministratorManager {
         try {
             return adminBean.getall();
         } catch (Exception e) {
+=======
+import ejbs.CuidadorBean;
+import ejbs.ProfissionalSaudeBean;
+import ejbs.UtenteBean;
+import entities.Cuidador;
+import entities.ProfissionalSaude;
+import entities.Utente;
+import java.util.List;
+import javax.ejb.EJB;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class AdministratorManager {
+   
+    @EJB
+    private UtenteBean utenteBean;
+    @EJB
+    private ProfissionalSaudeBean profissionalSaudeBean;
+    @EJB
+    private CuidadorBean cuidadorBean;
+    
+    private static final Logger logger = Logger.getLogger("web.AdministratorManager");
+    
+    
+    public List<Utente> getAllUtentes() {
+        try {
+            return utenteBean.getall();
+        } catch (Exception e) {
+            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", logger);
+        }
+        return null;
+    }
+    
+    public List<ProfissionalSaude> getAllProfissionais(){
+        try{
+            return profissionalSaudeBean.getAll();
+        }catch (Exception e) {
+            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", logger);
+        }
+         return null;
+    }
+    
+    public List<Cuidador> getAllCuidadores(){
+        try{
+            return cuidadorBean.getAll();
+        }catch (Exception e) {
+            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", logger);
+        }
+         return null;
+    }
+    
+    public  List<UtenteDTO> getAllUtentesDTO(){
+        try{
+            return utenteBean.getAllDTO();
+        }catch (Exception e) {
+            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", logger);
+        }
+        return null;
+    }
+    
+    public List<ProfissionalSaudeDTO> getAllProfissionaisDTO(){
+        try{
+            return profissionalSaudeBean.getAllDTO();
+        }catch (Exception e) {
+            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", logger);
+        }
+        return null;
+    }
+    
+    public List<CuidadorDTO> getAllCuidadoresDTO(){
+        try{
+            return cuidadorBean.getAllDTO();
+        }catch (Exception e) {
+>>>>>>> origin/master
             FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", logger);
         }
         return null;
